dcd94864075ef9fa067785cd2ad2c175c914877e
lucastrindade222 <lucas500000000@gmail.com>
2020-03-25 12:14:29 -0300

    teste da clase dimain

--- src/main/java/com/lucas/cursomc/resource/categoriaResource.java
+++ src/main/java/com/lucas/cursomc/resource/categoriaResource.java
@@ -1,17 +1,34 @@
 package com.lucas.cursomc.resource;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lucas.cursomc.domain.Categoria;
+
 @RestController
 @RequestMapping(value = "/categoria")
-public class categoriaResource {
+public class CategoriaResource {
 
 	
 	@RequestMapping(method = RequestMethod.GET)
-	public String listar() {
-		return "rest esta funma";
+	public List<Categoria> listar() {
+	 
+		Categoria c1 = new Categoria(1,"informatica");
+		Categoria c2 = new Categoria(2,"adimi");
+		
+		List<Categoria> lista = new ArrayList<>();
+		
+		
+		lista.add(c1);
+		lista.add(c2);
+		
+		return lista;
+		
+		
 	}
 	
 	
